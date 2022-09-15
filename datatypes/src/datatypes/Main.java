33c0de18467a48266eb37f9e14e46c8db6542f1c
Merve SARI <sarri.merrve@gmail.com>
2022-09-16 02:41:23 +0300

    Değişkenler ve değişken kullanım ihtiyacı

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,14 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi = 12 ;		
+		double sayi = 12.5;
+		
+		
+
+	}
+
+}
---
+++ variables/src/variables/Main.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int ögrenciSayisi = 24;
+		String mesaj = "Öğrenci sayısı : " ;
+		
+		System.out.println(mesaj + ögrenciSayisi);
+		System.out.println(mesaj + ögrenciSayisi);
+		System.out.println("Öğrenci sayısı : "+ögrenciSayisi);
+		System.out.println("Öğrenci sayısı : "+ögrenciSayisi);
+		System.out.println("Öğrenci sayısı : "+ögrenciSayisi);
+
+	}
+
+}
