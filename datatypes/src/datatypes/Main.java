39db5d3bd9aa362ac19b07acdaafdae32b8175e0
Merve SARI <sarri.merrve@gmail.com>
2022-09-17 23:41:42 +0300

    Temel Veri Tipleriyle Çalışmak

--- datatypes/src/datatypes/Main.java
+++ datatypes/src/datatypes/Main.java
@@ -4,10 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int sayi = 12 ;		
-		double sayi = 12.5;
+		//Primitive Types İlkel değer bazlı tipler
 		
-		
+		//boolean true or false, 1 byte not applicable
+		//char    single character 2 byte -128 to 127
+		//short	  integer          1 bytes -32768 to 32767
+		//int     integer          4 bytes  -2147483648 to 2147483647
+		//long    integer		   6 bytes 	 -9......808 to 9.......807
+		//float   floating-point number 4bytes -3.......x10*38 to -1......x10*-45
+		//double  floating-point number 8bytes 21.....x10*308 to 24.....x10*-324
+				
+				double sayi = 12.5;
+				sayi = -129;
+				
+				String sehir ="ANKARA";
+				char karakter = 'A';
+				
+				boolean dogruMu = true;
+				
 		
 
 	}
