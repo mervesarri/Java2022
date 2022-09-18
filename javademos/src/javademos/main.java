18167da740fa8ccb4cbf4c187bb6ae17d1ba99a0
Merve SARI <sarri.merrve@gmail.com>
2022-09-18 19:24:16 +0300

    Hello world

---
+++ javademos/src/javademos/main.java
@@ -0,0 +1,12 @@
+package javademos;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java 2022");
+
+	}
+
+}
