0b12515ffd1cd09e81fa6006b1fc6ad932296b10
Merve SARI <sarri.merrve@gmail.com>
2022-09-19 00:12:55 +0300

    Do-While döngüsü

---
+++ loopDemo2/src/loopDemo2/main.java
@@ -0,0 +1,15 @@
+package loopDemo2;
+
+public class main {
+
+	public static void main(String[] args) {
+//		Do-While
+		int i=13;
+		do{
+			System.out.println(i);
+			i++;
+		}while (i<10); 
+		    System.out.println("Döngü bitti");
+	}
+
+}
