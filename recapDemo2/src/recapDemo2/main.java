fca3f80aba3530293c400464edcf4727fc26eb3d
Merve SARI <sarri.merrve@gmail.com>
2022-09-19 02:32:06 +0300

    ReCap Demo 2

---
+++ recapDemo2/src/recapDemo2/main.java
@@ -0,0 +1,26 @@
+package recapDemo2;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		// double[] mylist = new double[4];
+		double[] myList = { 1.2, 6.3, 4.3, 5.6 };
+		double total = 0;
+		double max = myList[0];
+
+		for (double number : myList) {
+
+			if (max < number) {
+				max = number;
+			}
+
+			total = total + number;
+			System.out.println(number);
+		}
+		System.out.println("Toplam ="+total);
+		System.out.println("En buyuk sayı ="+max);
+
+	}
+
+}
