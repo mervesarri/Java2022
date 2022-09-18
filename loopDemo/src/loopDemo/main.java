0b47b904debea6b3641b02cb40f9e381fe813d1b
Merve SARI <sarri.merrve@gmail.com>
2022-09-18 23:50:55 +0300

    For Döngüsü

---
+++ loopDemo/src/loopDemo/main.java
@@ -0,0 +1,14 @@
+package loopDemo;
+
+public class main {
+
+	public static void main(String[] args) {
+//	for
+		for (int i=2;i<10;i+=2) {
+			System.out.println(i);
+			
+		}
+            System.out.println("Döngü bitti");
+	}  
+	
+}
