6cb64041ad861515f0d24a0b62c8f4b9a96097de
Merve SARI <sarri.merrve@gmail.com>
2022-09-18 23:59:16 +0300

    While döngüsü

---
+++ loopDemo1/src/loopDemo1/main.java
@@ -0,0 +1,16 @@
+package loopDemo1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// while
+		int i=1;
+		while (i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		
+		    System.out.println("Döngü bitti");
+	}
+
+}
