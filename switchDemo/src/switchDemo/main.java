bc4922deac58a9e0b1ea5b57b621b03ca03a112a
Merve SARI <sarri.merrve@gmail.com>
2022-09-18 19:44:38 +0300

    Switch Bloklarıyla Çalışmak

---
+++ switchDemo/src/switchDemo/main.java
@@ -0,0 +1,29 @@
+package switchDemo;
+
+public class main {
+
+	public static void main(String[] args) {
+		char grade = 'C';
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+			System.out.println("Çok iyi : Geçtiniz");
+			break;
+		case 'C':
+			System.out.println("İyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena Değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Malesef Kaldınız");
+			break;
+		default:
+			System.out.println("Geçersiz not girdiniz");
+		}
+
+	}
+
+}
