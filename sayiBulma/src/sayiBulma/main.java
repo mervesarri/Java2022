c0d903e4fe21c10f868a364a0edc293686d1f684
Merve SARI <sarri.merrve@gmail.com>
2022-09-19 19:51:05 +0300

    Sayı bulma

---
+++ sayiBulma/src/sayiBulma/main.java
@@ -0,0 +1,24 @@
+package sayiBulma;
+
+public class main {
+
+	public static void main(String[] args) {
+		int[] sayilar = new int[] { 1, 2, 5, 7, 9, 0 };
+		int aranacak = 22;
+		boolean varMi = false;
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+
+		if (varMi) {
+			System.out.println("Sayı mevcuttur");
+		} else {
+			System.out.println("Sayı mevcut değildir");
+		}
+
+	}
+
+}
