a8585c9adcec4ef33688dabfe800a75fc431d971
Merve SARI <sarri.merrve@gmail.com>
2022-09-19 19:35:50 +0300

    Mini Proje 2 - Kalın Sesli ve İnce Sesli Harfler

---
+++ vowels/src/vowels/main.java
@@ -0,0 +1,21 @@
+package vowels;
+
+public class main {
+
+	public static void main(String[] args) {
+		char harf = 'E';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın Sesli Harf");
+			break;
+		default:
+			System.out.println("İnce Sesli Harf");
+		}
+
+	}
+
+}
