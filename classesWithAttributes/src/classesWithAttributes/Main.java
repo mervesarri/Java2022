5aa2296c3f376cd67be9df5736246b1ca0292fab
Merve SARI <sarri.merrve@gmail.com>
2022-10-03 01:21:11 +0300

    Constructor ile çalışmak

--- classesWithAttributes/src/classesWithAttributes/Main.java
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -3,15 +3,20 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
+		// Product product2 = new Product(1, "Laptop", "Asus Laptop", 3000, 2, "Siyah");
+
 		Product product = new Product();
-		product.name = "Telefon";
-		product.id = 13;
-		product.description = "Samsung Telefon";
-		product.price = 8599;
-		product.stockAmount = 6;
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
 
 		ProductManager productManager = new ProductManager();
 		productManager.Add(product);
+
+		System.out.println(product.getKod());
+
 	}
 
 }
--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -3,6 +3,6 @@ package classesWithAttributes;
 public class ProductManager {
 	public void Add(Product product) {
 		// JDBC
-		System.out.println("Ürün Eklendi " + product.name);
+		System.out.println("Ürün Eklendi " + product.getName());
 	}
 }
