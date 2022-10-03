29e652affbd5643e7a705f79a6fce92adc82609f
Merve SARI <sarri.merrve@gmail.com>
2022-10-03 20:09:10 +0300

    Method Overloading

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,12 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.Topla(2, 3));
+		dortIslem.topla(2, 3, 5);
+
+	}
+
+}
