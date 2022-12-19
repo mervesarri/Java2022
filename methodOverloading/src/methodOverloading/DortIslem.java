699d24b1ba50d0e2d6a20dd85680cc6450c644fd
Merve SARI <sarri.merrve@gmail.com>
2022-12-19 12:05:42 +0300

    MethodOverloading
    Method Overloading ile çalışma

--- methodOverloading/src/methodOverloading/DortIslem.java
+++ methodOverloading/src/methodOverloading/DortIslem.java
@@ -5,8 +5,8 @@ public class DortIslem {
 		return sayi1 + sayi2;
 	}
 
-	public int topla(int sayi1, int sayi2, int sayi3) {
-		return sayi1 + sayi2;
+	public int Topla(int sayi1, int sayi2, int sayi3) {
+		return sayi1 + sayi2 + sayi3;
 	}
 
 }
--- methodOverloading/src/methodOverloading/Main.java
+++ methodOverloading/src/methodOverloading/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		DortIslem dortIslem = new DortIslem();
-		System.out.println(dortIslem.Topla(2, 3));
-		dortIslem.topla(2, 3, 5);
+		System.out.println("2+3=" + dortIslem.Topla(2, 3));
+		System.out.println("2+3+5=" + dortIslem.Topla(2, 3, 5));
 
 	}
 
